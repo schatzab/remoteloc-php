84b87b03c1159c545059ff561c3549ac96b57e71
Chelsea <runinbend@gmail.com>
2015-12-09 13:20:40 -0800

    initial setup

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
-<html>
+<!DOCTYPE html>
+<html lang="en" ng-app>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="Remote Locations" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="/images/favicon.ico">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script type="text/javascript" src="js/main.js"></script>
+<title>Remote Loc</title>
+</head>
+<body>
+<!-- header -->		
+<?php include("inc/header.inc.php"); ?></div> 
+
+<!-- footer -->	
+<?php include("inc/footer.inc.php"); ?></div> 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,2 @@
+/*primary JS file - chelsea town */
+

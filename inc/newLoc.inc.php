2767b6a76b0f02a71147fbe93104baccaffcb97d
Chelsea <runinbend@gmail.com>
2016-01-11 22:51:36 -0800

    final adjustments

--- inc/newLoc.inc.php
+++ inc/newLoc.inc.php
@@ -9,8 +9,8 @@
             <h2>Add a Remote Location</h2>
                 <form action="../index.php" method="post">
                     <input type="text" id="locName" class="text_field_generic" value="Business Name" onclick="if(this.value=='Business Name'){this.value=''}" onblur="if (this.value==''){this.value='Business Name'}"><br><br>
-                    <input type="text" name="comments" class="text_field_generic" value="Comments" onclick="if(this.value=='Comments'){this.value=''}" onblur="if (this.value==''){this.value='Comments'}"><br><br>
-                    <input type="submit" value="Submit">
+                    <textarea name="comments" class="text_field_generic" value="Comments" onclick="if(this.value=='Comments'){this.value=''}" onblur="if (this.value==''){this.value='Comments'}"></textarea><br><br>
+                    <input type="submit" value="ADD LOCATION" class="gen_button_prominent">
                     <input type="hidden" name="content" value="addLoc">
                     <!-- hidden variable sends to addLoc -->
                 </form>
